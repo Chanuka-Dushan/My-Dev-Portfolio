4065acb8b778e8a50000c1e7ab26b58a28853822
Dushan Chanuka <chanukadushan1030@gmail.com>
2025-04-13 22:54:45 +0530

    Configure GitHub Pages deployment

--- vite.config.ts
+++ vite.config.ts
@@ -18,6 +18,7 @@ export default defineConfig({
         ]
       : []),
   ],
+  base:"/My-Dev-Portfolio/",
   resolve: {
     alias: {
       "@": path.resolve(import.meta.dirname, "client", "src"),
